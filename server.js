683f894ac4e592ce1426cf0e215adf85c71de228
KartikeyCode <codekartikey@gmail.com>
2024-04-07 02:22:21 +0530

    check

--- server.js
+++ server.js
@@ -1,53 +1,16 @@
 const express = require("express");
-const cors = require("cors"); // Import cors module
+const http = require("http");
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"], // Allow only GET and POST requests
-  },
-});
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-const chatHistory = {};
+io.on("connection", socket => {
+    socket.emit("your id", socket.id);
+    socket.on("send message", body => {
+        io.emit("message", body)
+    })
+})
 
-app.use(cors()); // Enable CORS for all routes in Express app
 
-io.on("connection", (socket) => {
-  socket.emit("your id", socket.id);
-  
-  socket.on("join", (room) => {
-    socket.join(room);
-    console.log(`User joined room: ${room}`);
-
-    if (chatHistory[room]) {
-      socket.emit("chat history", chatHistory[room]);
-    }
-    
-    socket.on("leave", (room) => {
-      socket.leave(room);
-      console.log(`User left room: ${room}`);
-    });
-    
-    socket.on("chat message", (data) => {
-      const { room, message } = data;
-      console.log(`Message in room ${room} from ${socket.id}: ${message}`);
-      
-      if (!chatHistory[room]) {
-        chatHistory[room] = [];
-      }
-      chatHistory[room].push({ id: socket.id, body: message });
-      
-      io.to(room).emit("chat message", { id: socket.id, body: message });
-    });
-    
-    socket.on("disconnect", () => {
-      console.log("User disconnected");
-    });
-  });
-});
-  
-  const PORT = 5000;
-http.listen(PORT, () => {
-  console.log(`Listening on *:${PORT}`);
-});
+server.listen(8000, () => console.log("server is running on port 8000"));
