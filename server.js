bd180e0c264532ca67cc9e36d33de9afad5434b4
KartikeyCode <codekartikey@gmail.com>
2024-04-07 02:59:26 +0530

    pls work

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+
 const express = require("express");
 const cors = require("cors"); // Import cors module
+
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
@@ -14,8 +16,8 @@ const chatHistory = {};
 app.use(cors()); // Enable CORS for all routes in Express app
 
 io.on("connection", (socket) => {
-  socket.emit("your id", socket.id);
-  
+  console.log("A user connected");
+
   socket.on("join", (room) => {
     socket.join(room);
     console.log(`User joined room: ${room}`);
@@ -23,31 +25,31 @@ io.on("connection", (socket) => {
     if (chatHistory[room]) {
       socket.emit("chat history", chatHistory[room]);
     }
-    
-    socket.on("leave", (room) => {
-      socket.leave(room);
-      console.log(`User left room: ${room}`);
-    });
-    
-    socket.on("chat message", (data) => {
-      const { room, message } = data;
-      console.log(`Message in room ${room} from ${socket.id}: ${message}`);
-      
-      if (!chatHistory[room]) {
-        chatHistory[room] = [];
-      }
-      chatHistory[room].push({ id: socket.id, body: message });
-      
-      io.to(room).emit("chat message", { id: socket.id, body: message });
-    });
-    
-    socket.on("disconnect", () => {
-      console.log("User disconnected");
-    });
+  });
+
+  socket.on("leave", (room) => {
+    socket.leave(room);
+    console.log(`User left room: ${room}`);
+  });
+
+  socket.on("chat message", (data) => {
+    const { room, message } = data;
+    console.log(`Message in room ${room}: ${message}`);
+
+    if (!chatHistory[room]) {
+      chatHistory[room] = [];
+    }
+    chatHistory[room].push(message);
+
+    io.to(room).emit("chat message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
-  
-  const PORT = 5000;
+
+const PORT = 5000;
 http.listen(PORT, () => {
   console.log(`Listening on *:${PORT}`);
 });
